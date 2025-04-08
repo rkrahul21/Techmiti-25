35778bee99b066cec386bf52d2967903939c6c5a
Atul Raj <atul.dev13570@gmail.com>
2025-04-08 12:08:37 +0530

    Countdown Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,30 @@
-import React from "react";
-// hlh
+import React from 'react';
+import Countdown from './components/Countdown';
+
 function App() {
   const events = [
-    "RoboRace",
-    "Dangal",
-    "Quiz",
-    "Codingo",
-    "Hackathon",
-    "TechnoTalks",
-    "Workshops",
-    "Treasure Hunt",
-    "Gaming",
-    "Web Development",
-    "App Development",
-    "AI/ML",
+    "RoboRace", "Dangal", "Quiz", "Codingo", "Hackathon",
+    "TechnoTalks", "Workshops", "Treasure Hunt", "Gaming",
+    "Web Development", "App Development", "AI/ML"
   ];
+
   return (
     <>
-      <div className="bg-blue-900 text-white flex flex-col items-center justify-center h-screen">
+      <div className='bg-blue-900 text-white flex flex-col items-center justify-center h-screen'>
         <h1>TECHMIT'25</h1>
-        <h3>Coming Soon adil</h3>
+        <h3>Coming Soon</h3>
+        <Countdown />
       </div>
 
       <div>
         {events.map((event, index) => (
-          <div
-            key={index}
-            className="bg-blue-900 text-white flex flex-col items-center justify-center "
-          >
-            <h1 className="bg-yellow-500 m-2 p-2 rounded-md shadow-lg">
-              {event}
-            </h1>
+          <div key={index} className='bg-blue-900 text-white flex flex-col items-center justify-center'>
+            <h1 className='bg-yellow-500 m-2 p-2 rounded-md shadow-lg'>{event}</h1>
           </div>
         ))}
       </div>
+
+      
     </>
   );
 }
