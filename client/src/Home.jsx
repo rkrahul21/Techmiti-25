17bed50f804a10beb998f6eea434330cd4b419bf
rahul <krahul8197@gmail.com>
2025-04-09 17:14:29 +0530

    footer section

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import videoSrc from './assets/88fcbfc0.mp4';
 import AboutUs from './components/sections/AboutUs';
 import Countdown from './components/Countdown';
+import Footer from './components/Footer';
+import Sponsor from './components/sections/Sponsor';
 
 
 
@@ -14,13 +16,14 @@ function Home() {
                     className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
                     src={videoSrc}
                     autoPlay
-                    loop
                     muted
                     playsInline
-                />
+                /> 
             </div>
             <Countdown />
             <AboutUs />
+            <Sponsor />
+            <Footer/>
         </div>
     )
 }
