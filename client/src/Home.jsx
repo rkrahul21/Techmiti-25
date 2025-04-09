4e3c62e53bdb5d89ad81b3657bd4fbf6228c80b7
OM KUMAR <omkr01987@gmail.com>
2025-04-10 05:25:23 +0530

    Navbar updated

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,31 +1,28 @@
-import React from 'react'
-import videoSrc from './assets/88fcbfc0.mp4';
-import AboutUs from './components/sections/AboutUs';
-import Countdown from './components/Countdown';
-import Footer from './components/Footer';
-import Sponsor from './components/sections/Sponsor';
-
-
+import React from "react";
+import videoSrc from "./assets/88fcbfc0.mp4";
+import AboutUs from "./components/sections/AboutUs";
+import Countdown from "./components/Countdown";
+import Footer from "./components/Footer";
+import Sponsor from "./components/sections/Sponsor";
 
 function Home() {
-    return (
-        <div>
-
-            <div className="relative w-full h-screen overflow-hidden">
-                <video
-                    className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
-                    src={videoSrc}
-                    autoPlay
-                    muted
-                    playsInline
-                /> 
-            </div>
-            <Countdown />
-            <AboutUs />
-            <Sponsor />
-            <Footer/>
-        </div>
-    )
+  return (
+    <div min-h-screen bg-black>
+      <Countdown />
+      <div className="relative w-full h-screen overflow-hidden">
+        <video
+          className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
+          src={videoSrc}
+          autoPlay
+          muted
+          playsInline
+        />
+      </div>
+      <AboutUs />
+      <Sponsor />
+      <Footer />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
