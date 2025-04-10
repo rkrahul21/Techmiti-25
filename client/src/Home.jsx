7cf90687be5f6dbed56e6dd9a656873a514716b8
om <omkr01987@gmail.com>
2025-04-10 09:35:32 +0530

    Hero section updated

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import videoSrc from "./assets/88fcbfc0.mp4";
 import AboutUs from "./components/sections/AboutUs";
-import Countdown from "./components/Countdown";
+
 import Footer from "./components/Footer";
 import Sponsor from "./components/sections/Sponsor";
 import HeroSection from "./components/HeroSection";
@@ -10,16 +9,7 @@ function Home() {
   return (
     <div min-h-screen bg-black>
       <HeroSection />
-      <Countdown />
-      <div className="relative w-full h-screen overflow-hidden">
-        <video
-          className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
-          src={videoSrc}
-          autoPlay
-          muted
-          playsInline
-        />
-      </div>
+
       <AboutUs />
       <Sponsor />
       <Footer />
